6b6adf799a613b47232529c0f713bb5ced3d3144
nathanallen <thenathanator@gmail.com>
2016-10-16 16:10:03 -0700

    valid json, fix links

--- data.js
+++ data.js
@@ -1,99 +1,93 @@
 window.data = {
-  projects: [
+  "projects": [
     {
-      title: "Raise The Error",
-      homepage_url: "/raise-the-error/",
-      github_url: "https://github.com/nathanallen/raise-the-error",
-      description: "Can you reproduce errors in javascript?"
+      "title": "Raise The Error",
+      "homepage_url": "/raise-the-error",
+      "github_url": "https://github.com/nathanallen/raise-the-error",
+      "description": "Can you reproduce errors in javascript?"
     },
     {
       "title": "bikegnosis",
-      "homepage_url": "bikegnosis",
-      "github_url": "bikegnosis",
-      "description": "bikegnosis"
+      "homepage_url": "/bikegnosis",
+      "github_url": "https://github.com/nathanallen/bikegnosis",
+      "description": "What's wrong with your bike?"
     },
     {
       "title": "blinking-cursor",
-      "homepage_url": "blinking-cursor",
-      "github_url": "blinking-cursor",
+      "homepage_url": "/blinking-cursor",
+      "github_url": "https://github.com/nathanallen/blinking-cursor",
       "description": "blinking-cursor"
     },
     {
       "title": "collapse",
-      "homepage_url": "collapse",
-      "github_url": "collapse",
+      "homepage_url": "/collapse",
+      "github_url": "https://github.com/nathanallen/collapse",
       "description": "collapse"
     },
     {
-      "title": "foodunites.us",
-      "homepage_url": "foodunites.us",
-      "github_url": "foodunites.us",
-      "description": "foodunites.us"
-    },
-    {
       "title": "geoquakes",
-      "homepage_url": "geoquakes",
-      "github_url": "geoquakes",
+      "homepage_url": "/geoquakes",
+      "github_url": "https://github.com/nathanallen/geoquakes",
       "description": "geoquakes"
     },
     {
       "title": "giffaw",
-      "homepage_url": "giffaw",
-      "github_url": "giffaw",
+      "homepage_url": "/giffaw",
+      "github_url": "https://github.com/nathanallen/giffaw",
       "description": "giffaw"
     },
     {
       "title": "mouse-a-sketch",
-      "homepage_url": "mouse-a-sketch",
-      "github_url": "mouse-a-sketch",
+      "homepage_url": "/mouse-a-sketch/mouse_a_sketch",
+      "github_url": "https://github.com/nathanallen/mouse-a-sketch",
       "description": "mouse-a-sketch"
     },
     {
       "title": "movie-finder",
-      "homepage_url": "movie-finder",
-      "github_url": "movie-finder",
+      "homepage_url": "/movie-finder/app",
+      "github_url": "https://github.com/nathanallen/movie-finder",
       "description": "movie-finder"
     },
     {
       "title": "nathanallen.github.io",
-      "homepage_url": "nathanallen.github.io",
-      "github_url": "nathanallen.github.io",
+      "homepage_url": "/nathanallen.github.io",
+      "github_url": "https://github.com/nathanallen/nathanallen.github.io",
       "description": "nathanallen.github.io"
     },
     {
       "title": "protofisk",
-      "homepage_url": "protofisk",
-      "github_url": "protofisk",
+      "homepage_url": "/protofisk/public",
+      "github_url": "https://github.com/nathanallen/protofisk",
       "description": "protofisk"
     },
     {
       "title": "quotefully",
-      "homepage_url": "quotefully",
-      "github_url": "quotefully",
+      "homepage_url": "/quotefully",
+      "github_url": "https://github.com/nathanallen/quotefully",
       "description": "quotefully"
     },
     {
       "title": "sanity-checker",
-      "homepage_url": "sanity-checker",
-      "github_url": "sanity-checker",
+      "homepage_url": "/sanity-checker/test",
+      "github_url": "https://github.com/nathanallen/sanity-checker",
       "description": "sanity-checker"
     },
     {
       "title": "search",
-      "homepage_url": "search",
-      "github_url": "search",
+      "homepage_url": "/search",
+      "github_url": "https://github.com/nathanallen/search",
       "description": "search"
     },
     {
       "title": "tictactoe",
-      "homepage_url": "tictactoe",
-      "github_url": "tictactoe",
+      "homepage_url": "/tictactoe",
+      "github_url": "https://github.com/nathanallen/tictactoe",
       "description": "tictactoe"
     },
     {
       "title": "tic_tac_toe",
-      "homepage_url": "tic_tac_toe",
-      "github_url": "tic_tac_toe",
+      "homepage_url": "/tic_tac_toe",
+      "github_url": "https://github.com/nathanallen/tic_tac_toe",
       "description": "tic_tac_toe"
     }
   ]
