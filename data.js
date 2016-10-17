cd721f9f745188c086925d26ac77cd711cd69a72
nathanallen <thenathanator@gmail.com>
2016-10-17 13:28:22 -0700

    update json

--- data.js
+++ data.js
@@ -4,91 +4,79 @@ window.data = {
       "title": "raise-the-error",
       "homepage_url": "/raise-the-error",
       "github_url": "https://github.com/nathanallen/raise-the-error",
-      "description": "Can you reproduce errors in javascript?"
+      "description": "Makes a game out of reproducing common Javascript errors."
     },
     {
       "title": "bikegnosis",
       "homepage_url": "/bikegnosis",
       "github_url": "https://github.com/nathanallen/bikegnosis",
-      "description": "What's wrong with your bike?"
-    },
-    {
-      "title": "blinking-cursor",
-      "homepage_url": "/blinking-cursor",
-      "github_url": "https://github.com/nathanallen/blinking-cursor",
-      "description": "blinking-cursor"
-    },
-    {
-      "title": "collapse",
-      "homepage_url": "/collapse",
-      "github_url": "https://github.com/nathanallen/collapse",
-      "description": "collapse"
+      "description": "Prototype for a bicycle diagnostic tool. Narrows down what might be wrong with a bicycle, given the noises its making."
     },
     {
       "title": "geoquakes",
       "homepage_url": "/geoquakes",
       "github_url": "https://github.com/nathanallen/geoquakes",
-      "description": "geoquakes"
+      "description": "Mashup of Google Maps and earthquake readings from the past week. AJAX learning lab."
     },
     {
       "title": "giffaw",
       "homepage_url": "/giffaw",
       "github_url": "https://github.com/nathanallen/giffaw",
-      "description": "giffaw"
+      "description": "Search for gifs using the Giphy API. AJAX learning lab."
     },
     {
       "title": "mouse-a-sketch",
       "homepage_url": "/mouse-a-sketch/mouse_a_sketch",
       "github_url": "https://github.com/nathanallen/mouse-a-sketch",
-      "description": "mouse-a-sketch"
+      "description": "Simple drawing app using event listeners. jQuery review lab."
+    },
+    {
+      "title": "collapse",
+      "homepage_url": "/collapse",
+      "github_url": "https://github.com/nathanallen/collapse",
+      "description": "Fun with randomized colors and collapsing towers of blocks. Purely experimental."
     },
     {
       "title": "movie-finder",
       "homepage_url": "/movie-finder/app",
       "github_url": "https://github.com/nathanallen/movie-finder",
-      "description": "movie-finder"
-    },
-    {
-      "title": "nathanallen.github.io",
-      "homepage_url": "/nathanallen.github.io",
-      "github_url": "https://github.com/nathanallen/nathanallen.github.io",
-      "description": "nathanallen.github.io"
+      "description": "Search for movies using the OMDB API."
     },
     {
       "title": "protofisk",
       "homepage_url": "/protofisk/public",
       "github_url": "https://github.com/nathanallen/protofisk",
-      "description": "protofisk"
+      "description": "Prototype of a sentence-level article annotation and tagging tool."
     },
     {
       "title": "quotefully",
       "homepage_url": "/quotefully",
       "github_url": "https://github.com/nathanallen/quotefully",
-      "description": "quotefully"
+      "description": "Prototype of a highlightable and collapsable quote."
     },
     {
       "title": "sanity-checker",
       "homepage_url": "/sanity-checker/test",
       "github_url": "https://github.com/nathanallen/sanity-checker",
-      "description": "sanity-checker"
+      "description": "Module intended to alert students to the fact that they have an error in their Javascript Console. This dashboard allows for manual configuration/testing of various error types."
     },
     {
       "title": "search",
       "homepage_url": "/search",
       "github_url": "https://github.com/nathanallen/search",
-      "description": "search"
+      "description": "Simple search interface that only displays results from trustworthy sources like MDN and Stackoverflow. It also performs some naive checks that e.g. a programming language is specified."
     },
     {
-      "title": "tictactoe",
-      "homepage_url": "/tictactoe",
-      "github_url": "https://github.com/nathanallen/tictactoe",
-      "description": "tictactoe"
+      "title": "blinking-cursor",
+      "homepage_url": "/blinking-cursor",
+      "github_url": "https://github.com/nathanallen/blinking-cursor",
+      "description": "Because life is better with &lt;blink&gt; tags."
     },
     {
       "title": "tic_tac_toe",
       "homepage_url": "/tic_tac_toe",
       "github_url": "https://github.com/nathanallen/tic_tac_toe",
-      "description": "tic_tac_toe"
+      "description": "An OO game of Tic Tac Toe."
     }
   ]
 }
