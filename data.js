2bc1af8b4f6b5063d3eaf42f2034b22e7bef1880
nathanallen <thenathanator@gmail.com>
2017-07-26 09:09:09 -0700

    fix embed by removing duplicate leading slash

--- data.js
+++ data.js
@@ -3,154 +3,154 @@ window.data = {
     {
       "title": "raise-the-error",
       "year": 2016,
-      "homepage_url": "/raise-the-error",
+      "homepage_url": "raise-the-error",
       "github_url": "https://github.com/nathanallen/raise-the-error",
       "description": "Makes a game out of reproducing common Javascript errors."
     },
     {
       "title": "emoji-finder",
       "year": 2017,
-      "homepage_url": "/emoji-finder",
+      "homepage_url": "emoji-finder",
       "github_url": "https://github.com/nathanallen/emoji-finder",
       "description": "Built for a 1-day intro to web development workshop. Filters emoji by tag."
     },
     {
       "title": "bikegnosis",
       "year": 2014,
-      "homepage_url": "/bikegnosis",
+      "homepage_url": "bikegnosis",
       "github_url": "https://github.com/nathanallen/bikegnosis",
       "description": "Prototype for a bicycle diagnostic tool. Narrows down what might be wrong with a bicycle, given the noises its making."
     },
     {
       "title": "geoquakes",
       "year": 2015,
-      "homepage_url": "/geoquakes",
+      "homepage_url": "geoquakes",
       "github_url": "https://github.com/nathanallen/geoquakes",
       "description": "Mashup of Google Maps and earthquake readings from the past week. AJAX learning lab."
     },
     {
       "title": "racing_game",
       "year": 2016,
-      "homepage_url": "/racing_game",
+      "homepage_url": "racing_game",
       "github_url": "https://github.com/nathanallen/racing_game",
       "description": "A fully OO jquery racing game. The six-player demo uses keys 0-5 for movement."
     },
     {
       "title": "giffaw",
       "year": 2015,
-      "homepage_url": "/giffaw",
+      "homepage_url": "giffaw",
       "github_url": "https://github.com/nathanallen/giffaw",
       "description": "Search for gifs using the Giphy API. AJAX learning lab."
     },
     {
       "title": "mouse-a-sketch",
       "year": 2015,
-      "homepage_url": "/mouse-a-sketch/mouse_a_sketch",
+      "homepage_url": "mouse-a-sketch/mouse_a_sketch",
       "github_url": "https://github.com/nathanallen/mouse-a-sketch",
       "description": "Simple drawing app using event listeners. jQuery review lab."
     },
     {
       "title": "color-me-angular",
       "year": 2016,
-      "homepage_url": "/color-me-angular",
+      "homepage_url": "color-me-angular",
       "github_url": "https://github.com/nathanallen/color-me-angular",
       "description": "Explore 148 CSS color names. Angular.js routing lab."
     },
     {
       "title": "symbol-grid",
       "year": 2017,
-      "homepage_url": "/creative-code-sketches/symbol-grid",
+      "homepage_url": "creative-code-sketches/symbol-grid",
       "github_url": "https://github.com/creative-code-sketches",
       "description": "An endless cycling grid of unicode characters."
     },
     {
       "title": "singing-bowls",
       "year": 2017,
-      "homepage_url": "/singing-bowls",
+      "homepage_url": "singing-bowls",
       "github_url": "https://github.com/singing-bowls",
       "description": "Interactive exploration of the resonant frequencies of physical objects."
     },
     {
       "title": "paint-sentence",
       "year": 2017,
-      "homepage_url": "/creative-code-sketches/paint-sentence",
+      "homepage_url": "creative-code-sketches/paint-sentence",
       "github_url": "https://github.com/creative-code-sketches",
       "description": "Paint sentences on a p5js canvas."
     },
     {
       "title": "boom-box",
       "year": 2017,
-      "homepage_url": "/creative-code-sketches/boom-box",
+      "homepage_url": "creative-code-sketches/boom-box",
       "github_url": "https://github.com/creative-code-sketches",
       "description": "Interactive WebAudio visualization with p5js"
     },
     {
       "title": "de-stijl-diorama",
       "year": 2017,
-      "homepage_url": "/de-stijl-diorama",
+      "homepage_url": "de-stijl-diorama",
       "github_url": "https://github.com/nathanallen/de-stijl-diorama",
       "description": "Experimental 2d diorama effect on window resize. Based on an illustration by Gouden Vlinders Franke."
     },
     {
       "title": "collapse",
       "year": 2014,
-      "homepage_url": "/collapse",
+      "homepage_url": "collapse",
       "github_url": "https://github.com/nathanallen/collapse",
       "description": "Fun with randomized colors and collapsing towers of blocks. Purely experimental."
     },
     {
       "title": "movie-finder",
       "year": 2015,
-      "homepage_url": "/movie-finder/app",
+      "homepage_url": "movie-finder/app",
       "github_url": "https://github.com/nathanallen/movie-finder",
       "description": "Search for movies using the OMDB API."
     },
     {
       "title": "protofisk",
       "year": 2015,
-      "homepage_url": "/protofisk/public",
+      "homepage_url": "protofisk/public",
       "github_url": "https://github.com/nathanallen/protofisk",
       "description": "Prototype of a sentence-level article annotation and tagging tool."
     },
     {
       "title": "quotefully",
       "year": 2014,
-      "homepage_url": "/quotefully",
+      "homepage_url": "quotefully",
       "github_url": "https://github.com/nathanallen/quotefully",
       "description": "Prototype of a highlightable and collapsable quote."
     },
     {
       "title": "sanity-checker",
       "year": 2016,
-      "homepage_url": "/sanity-checker/test",
+      "homepage_url": "sanity-checker/test",
       "github_url": "https://github.com/nathanallen/sanity-checker",
       "description": "Module intended to alert students to the fact that they have an error in their Javascript Console. This dashboard allows for manual configuration/testing of various error types."
     },
     {
       "title": "search",
       "year": 2014,
-      "homepage_url": "/search",
+      "homepage_url": "search",
       "github_url": "https://github.com/nathanallen/search",
       "description": "Simple search interface that only displays results from trustworthy sources like MDN and Stackoverflow. It also performs some naive checks that e.g. a programming language is specified."
     },
     {
       "title": "blinking-cursor",
       "year": 2013,
-      "homepage_url": "/blinking-cursor",
+      "homepage_url": "blinking-cursor",
       "github_url": "https://github.com/nathanallen/blinking-cursor",
       "description": "Because life is better with &lt;blink&gt; tags."
     },
     {
       "title": "tic_tac_toe",
       "year": 2015,
-      "homepage_url": "/tic_tac_toe",
+      "homepage_url": "tic_tac_toe",
       "github_url": "https://github.com/nathanallen/tic_tac_toe",
       "description": "An OO game of Tic Tac Toe."
     },
     {
       "title": "iframe-inception",
       "year": 2016,
-      "homepage_url": "/iframe-inception",
+      "homepage_url": "iframe-inception",
       "github_url": "https://github.com/nathanallen/iframe-inception",
       "description": "Demonstrates a recursive scenario in which a webpage loads an iframe pointing at itself. Because inception ¯\\_(ツ)_/¯"
     }
